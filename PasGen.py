38fc73b5a1b4a56df1488c9077a8d789f4a24db1
Marc <marc.montalvez@gmail.com>
2021-11-28 21:14:00 +0100

    V01

---
+++ PasGen.py
@@ -0,0 +1,21 @@
+#importacion de depndencias
+import random
+
+#valores que constituyen la contraseña
+minus = "abcdefghijklmnopqrstuvwxyz"
+mayus = minus.upper()
+numeros = "0123456789"
+#En caso de tener que borrar carácteres, para recuperarlos
+#utilizar el siguiente comentario:
+#",;.:-_´¨`^+*?'>¿º\<¡*%$}€#""=[]{+/"
+simbolos =",;.:-_´¨`^+*?'>¿º\<¡*%$}€#""=[]{+/"
+base = minus+mayus+numeros+simbolos
+
+#inputs
+longitud=32 #el input no puede ser mayor que la longitud de "base"
+#print(len(base))
+
+muestra = random.sample(base,longitud)
+password = "".join(muestra)
+print(password)
+
